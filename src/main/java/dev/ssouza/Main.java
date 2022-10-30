8fce798a3b13781b5168329a5d89ef9baa555025
Samuel Souza <samuelsouza.contact@gmail.com>
2022-10-30 17:48:31 -0300

    cleaning up

--- src/main/java/dev/ssouza/Main.java
+++ src/main/java/dev/ssouza/Main.java
@@ -8,8 +8,6 @@ import org.pcap4j.core.PcapNativeException;
 import java.net.UnknownHostException;
 
 public class Main {
-    private static final int COUNT = 5;
-
     public static void main(String[] args) throws PcapNativeException, UnknownHostException, NotOpenException {
         final Publisher publisher = new ConsolePublisher();
         final Sniffer sniffer = new Sniffer(publisher);
